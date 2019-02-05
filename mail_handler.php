232a0334981a8678b2388ef2091d4e3a4fa64848
GabrielTopciu <lostglory15@gmail.com>
2019-02-05 21:02:55 +0000

    test03

--- mail_handler.php
+++ mail_handler.php
@@ -1,20 +1,19 @@
 <?php
-if(isset($_POST['submit'])) {
-$name=$_POST['name']
-$email=$_POST['email']
-$phone=$_POST['phone']
-$msg=$_POST['msg'];
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+		$email=$_POST['email'];
+		$msg=$_POST['msg'];
 
-$to='lostglory15@gmail.com';
-$subject='Form Submission';
-$message="Name: "$name. "\n"."Phone": ".$phone". "\n". "Wrote the following : "."\n\n".msg$;
-$headers=From: ".$email";
+		$to='lostglory15@gmail.com'; 
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
 
-if(mail($to, $subject, $message, $headers)) {
-    echo"<h1>Sent Successfully! Thank you"." ".$name.", I will reply shortly!</h1>";
-}
-else{
-    echo "Something went wrong, ";
-}
-}
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", I will reply to you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
 ?>
